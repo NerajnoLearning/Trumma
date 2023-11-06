aba1805cfa4aef351e6097cfa4eac716b6e034c0
Nerando Johnson <nerajno@gmail.com>
2023-11-05 20:54:37 -0500

    still broken

--- src/components/DrumMachine.tsx
+++ src/components/DrumMachine.tsx
@@ -3,15 +3,15 @@ import DrumPad from "./DrumPad";
 
 
 const DRUM_PADS = [
-  { triggerKey: "Q", audioClip: "../assets/audio/Clap.mp3"},
-  { triggerKey: "W", audioClip: "../assets/audio/Closed-HH.mp3" },
-  { triggerKey: "E", audioClip: "../assets/audio/Heater-1.mp3" },
-  { triggerKey: "A", audioClip: "../assets/audio/Heater-2.mp3" },
-  { triggerKey: "S", audioClip: "../assets/audio/Heater-3.mp3" },
-  { triggerKey: "D", audioClip: "../assets/audio/Heater-4.mp3" },
-  { triggerKey: "Z", audioClip: "../assets/audio/Kick_n_Hat.mp3" },
-  { triggerKey: "X", audioClip: "../assets/audio/Kick.mp3" },
-  { triggerKey: "C", audioClip: "../assets/audio/Open-HH.mp3" },
+  { triggerKey: "Q", audioClip: "../assets/audio/Clap.mp3", className: "clip", id:"Q"},
+  { triggerKey: "W", audioClip: "../assets/audio/Closed-HH.mp3", className: "clip", id:"W" },
+  { triggerKey: "E", audioClip: "../assets/audio/Heater-1.mp3", className: "clip", id:"E" },
+  { triggerKey: "A", audioClip: "../assets/audio/Heater-2.mp3", className: "clip", id:"A" },
+  { triggerKey: "S", audioClip: "../assets/audio/Heater-3.mp3", className: "clip", id:"S" },
+  { triggerKey: "D", audioClip: "../assets/audio/Heater-4.mp3", className: "clip", id:"D" },
+  { triggerKey: "Z", audioClip: "../assets/audio/Kick_n_Hat.mp3", className: "clip", id:"Z" },
+  { triggerKey: "X", audioClip: "../assets/audio/Kick.mp3", className: "clip", id:"X" },
+  { triggerKey: "C", audioClip: "../assets/audio/Open-HH.mp3", className: "clip", id:"C" },
 ];
 
 const DrumMachine: React.FC = () => {
@@ -21,7 +21,7 @@ const DrumMachine: React.FC = () => {
       {DRUM_PADS.map((drumPad) => (
         <DrumPad
           key={drumPad.triggerKey.valueOf()}
-          id={drumPad.triggerKey}
+          id={drumPad.triggerKey.valueOf()}
           triggerKey={drumPad.triggerKey}
           audioClip={drumPad.audioClip }
         />
