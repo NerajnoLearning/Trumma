e205c5a09d4be34f8c85f9263ea9c567795a7ea5
Nerando Johnson <nerajno@gmail.com>
2024-01-07 12:26:00 -0500

    draft v - beta

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
 import { AudioClip } from './types';
+import Drum from './Drum';
 
-const AudioClips: AudioClip[] = [
+const audioClips: AudioClip[] = [
     {
         keyTrigger: 'Q',
         url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
@@ -53,11 +54,29 @@ const AudioClips: AudioClip[] = [
 // Remove the unused variables count and setCount
 // Move the useState hook inside the App component
 const App: React.FC = () => {
-    const [ count, setCount ] = useState(0);
+
+    const playAudio = (e: React.KeyboardEvent<HTMLDivElement>) => {
+        const clip = audioClips.find((clip) => clip.keyTrigger === e.key.toUpperCase());
+        if (!clip) return;
+        (document.getElementById(clip.keyTrigger) as HTMLAudioElement)
+        .play()
+        .catch(console.error);
+
+        document.getElementById('drum-' + clip.keyTrigger)?.focus();
+        document.getElementById('display')!.innerText = clip.description;
+    }
+
+
 
     return (
-        <div className="container" id="drum-machine">
-            {/* Add your code here */}
+        <div className="container" id="drum-machine" onKeyDown={playAudio}>
+            <h1>freeCode Camp Drum Machine</h1>
+            <div className="whole-drum">
+                {audioClips.map((clip) => (
+                    <Drum audioClip={clip} key={clip.keyTrigger} />
+                ))}
+            </div>
+            <div id="display"></div>
         </div>
     );
 };
